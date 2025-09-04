0bf1636b28c633cf37354218ff51d9b6dd61b3e8
space cader <emmaleonard3008@gmail.com>
2025-09-04 20:42:41 +0300

    another one

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -100,12 +100,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'YdnuOWMiwstXGJtxYyTJmrOBeIVzLcok',
+        'PASSWORD': 'tVdmENICXcdaiUSiTvCYTGUfGymraMtr',
         'HOST': 'postgres.railway.internal',
         'PORT': '5432'
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
