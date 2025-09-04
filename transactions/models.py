cca24de97cb8dc8ed33d54c71eccf329e8f1e61a
space cader <emmaleonard3008@gmail.com>
2025-09-04 22:39:50 +0300

    models commit

--- transactions/models.py
+++ transactions/models.py
@@ -136,7 +136,7 @@ class Purchase(models.Model):
         max_digits=10,
         decimal_places=2,
         default=0.0,
-        verbose_name="Price per item (Ksh)",
+        verbose_name="Price per item (Tsh)",
     )
     total_value = models.DecimalField(max_digits=10, decimal_places=2)
 
